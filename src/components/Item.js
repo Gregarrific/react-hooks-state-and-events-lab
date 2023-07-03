43f7248baa5315a39fcca1c58e3902087bd84580
Gregory Dutton <greg.dutton@gmail.com>
2023-07-03 12:48:04 -0700

    Add state change to track items in the cart

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [inCart, setCart] = useState(false);
+  const inCartClass = inCart ? "in-cart" : "";
+  const cartBtnText = inCart ? "Remove From Cart" : "Add to Cart";
+
+  function handleCart() {
+    setCart(inCart => !inCart);
+  }
   return (
-    <li className="">
+    <li className={inCartClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleCart}>{cartBtnText}</button>
     </li>
   );
 }
